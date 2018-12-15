dd0fec626489451e92b2925a0aab60a77e885ee3
jordynmcginnis <jordynm4@gmail.com>
2018-12-15 11:02:47 -0700

    added useBattery hook

--- src/hooks/useBattery.js
+++ src/hooks/useBattery.js
@@ -1,8 +1,61 @@
-const name = ``;
+const name = `useBattery`;
 
-const implementation = ``;
+const implementation = `const on = (obj, ...args) => obj.addEventListener(...args);
+const off = (obj, ...args) => obj.removeEventListener(...args);
 
-const usage = ``;
+const useBattery = () => {
+  const [state, setState] = useState({});
+  let mounted = true;
+  let battery = null;
+
+  const onChange = () => {
+    const { charging, level, chargingTime, dischargingTime } = battery;
+    setState({
+      charging,
+      level,
+      chargingTime,
+      dischargingTime
+    });
+  };
+
+  const onBattery = () => {
+    onChange();
+    on(battery, "chargingchange", onChange);
+    on(battery, "levelchange", onChange);
+    on(battery, "chargingtimechange", onChange);
+    on(battery, "dischargingtimechange", onChange);
+  };
+
+  useEffect(
+    () => {
+      navigator.getBattery().then(bat => {
+        if (mounted) {
+          battery = bat;
+          onBattery();
+        }
+      });
+
+      return () => {
+        mounted = false;
+        if (battery) {
+          off(battery, "chargingchange", onChange);
+          off(battery, "levelchange", onChange);
+          off(battery, "chargingtimechange", onChange);
+          off(battery, "dischargingtimechange", onChange);
+        }
+      };
+    },
+    [0]
+  );
+
+  return state;
+};`;
+
+const usage = `function Demo() {
+  const state = useBattery();
+
+  return <pre>{JSON.stringify(state, null, 2)}</pre>;
+}`;
 
 export default {
   name,
