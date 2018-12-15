8728412c7d05b0106cf2c179c62aa87141868d79
jordynmcginnis <jordynm4@gmail.com>
2018-12-15 11:50:35 -0700

    added name and usage

--- src/hooks/useNetworkStatus.js
+++ src/hooks/useNetworkStatus.js
@@ -1,8 +1,40 @@
-const name = ``;
+const name = `useNetworkStatus`;
 
-const implementation = ``;
+const implementation = `function getConnection() {
+  return (
+    navigator.connection ||
+    navigator.mozConnection ||
+    navigator.webkitConnection
+  );
+}
 
-const usage = ``;
+function useNetworkStatus() {
+  let [connection, updateNetworkConnection] = useState(getConnection());
+
+  function updateConnectionStatus() {
+    updateNetworkConnection(getConnection());
+  }
+  useEffect(() => {
+    connection.addEventListener("change", updateConnectionStatus);
+    return () => {
+      connection.removeEventListener("change", updateConnectionStatus);
+    };
+  }, []);
+
+  return connection;
+}`;
+
+const usage = `function Demo() {
+  let connection = useNetworkStatus();
+  return (
+    <div>
+      <div>downlink: {connection.downlink}</div>
+      <div>effectiveType: {connection.effectiveType}</div>
+      <div>rtt: {connection.rtt}</div>
+      <div>saveData: {connection.saveData ? "yes" : "no"}</div>
+    </div>
+  );
+}`;
 
 export default {
   name,
