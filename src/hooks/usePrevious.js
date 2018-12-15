48feceaecb941843a3e17b2f93dbbf339638d336
jordynmcginnis <jordynm4@gmail.com>
2018-12-15 16:28:28 -0700

    added usePrevious hook

---
+++ src/hooks/usePrevious.js
@@ -0,0 +1,25 @@
+const name = `usePrevious`;
+
+const implementation = `usePrevious = value => {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+};`;
+
+const usage = `const Counter = () => {
+  const [count, setCount] = useState(0);
+  const prevCount = usePrevious(count);
+  return (
+    <h1>
+      Now: {count}, before: {prevCount}
+    </h1>
+  );
+};`;
+
+export default {
+  name,
+  implementation,
+  usage
+}
