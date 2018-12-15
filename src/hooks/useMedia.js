9c95be9441e2e9ca8183898f7f651752e72c25a8
jordynmcginnis <jordynm4@gmail.com>
2018-12-15 11:21:14 -0700

    added usage

--- src/hooks/useMedia.js
+++ src/hooks/useMedia.js
@@ -1,8 +1,36 @@
 const name = `useMedia`;
 
-const implementation = ``;
+const implementation = `const useMedia = (query, defaultState = false) => {
+  const [state, setState] = useState(defaultState);
 
-const usage = ``;
+  useEffect(
+    () => {
+      let mounted = true;
+      const mql = window.matchMedia(query);
+      const onChange = () => {
+        if (!mounted) return;
+        setState(!!mql.matches);
+      };
+
+      mql.addListener(onChange);
+      setState(mql.matches);
+
+      return () => {
+        mounted = false;
+        mql.removeListener(onChange);
+      };
+    },
+    [query]
+  );
+
+  return state;
+};`;
+
+const usage = `function Demo() {
+  const isWide = useMedia("(min-width: 480px)");
+
+  return <div>Screen is wide: {isWide ? "Yes" : "No"}</div>;
+}`;
 
 export default {
   name,
