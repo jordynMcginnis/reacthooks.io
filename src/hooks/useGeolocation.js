2ce7ee3d4ea8ebd5541ebe64f4dc16e8180e3a99
jordynmcginnis <jordynm4@gmail.com>
2018-12-15 11:05:18 -0700

    added useGeo hook

--- src/hooks/useGeolocation.js
+++ src/hooks/useGeolocation.js
@@ -1,8 +1,55 @@
-const name = ``;
+const name = `useGeolocation`;
 
-const implementation = ``;
+const implementation = `const useGeolocation = () => {
+  const [state, setState] = useState({
+    accuracy: null,
+    altitude: null,
+    altitudeAccuracy: null,
+    heading: null,
+    latitude: null,
+    longitude: null,
+    speed: null,
+    timestamp: Date.now()
+  });
+  let mounted = true;
+  let watchId;
 
-const usage = ``;
+  const onEvent = event => {
+    if (mounted) {
+      setState({
+        accuracy: event.coords.accuracy,
+        altitude: event.coords.altitude,
+        altitudeAccuracy: event.coords.altitudeAccuracy,
+        heading: event.coords.heading,
+        latitude: event.coords.latitude,
+        longitude: event.coords.longitude,
+        speed: event.coords.speed,
+        timestamp: event.timestamp
+      });
+    }
+  };
+
+  useEffect(
+    () => {
+      navigator.geolocation.getCurrentPosition(onEvent);
+      watchId = navigator.geolocation.watchPosition(onEvent);
+
+      return () => {
+        mounted = false;
+        navigator.geolocation.clearWatch(watchId);
+      };
+    },
+    [0]
+  );
+
+  return state;
+};`;
+
+const usage = `function Demo() {
+  const state = useGeolocation();
+
+  return <pre>{JSON.stringify(state, null, 2)}</pre>;
+}`;
 
 export default {
   name,
