eb88f0b0f63936e3edeb9021750d96a940e17a1f
jordynmcginnis <jordynm4@gmail.com>
2018-12-15 11:40:07 -0700

    added name

--- src/hooks/useTitle.js
+++ src/hooks/useTitle.js
@@ -1,8 +1,19 @@
-const name = `use`;
+const name = `useTitle`;
 
-const implementation = ``;
+const implementation = `const useTitle = title => {
+  useEffect(
+    () => {
+      document.title = title;
+    },
+    [title]
+  );
+};`;
 
-const usage = ``;
+const usage = `function Demo() {
+  useTitle("Hello world!");
+
+  return null;
+}`;
 
 export default {
   name,
