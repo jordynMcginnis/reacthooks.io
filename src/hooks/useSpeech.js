91419ea9208d20443f41a86bd9dfa9662faaf361
jordynmcginnis <jordynm4@gmail.com>
2018-12-15 11:31:28 -0700

    added new files

--- src/hooks/useSpeech.js
+++ src/hooks/useSpeech.js
@@ -1,8 +1,43 @@
 const name = `useSpeech`;
 
-const implementation = ``;
+const implementation = `const useSetState = (initialState = {}) => {
+  const [state, set] = useState(initialState);
+  const setState = patch => {
+    Object.assign(state, patch);
+    set(state);
+  };
 
-const usage = ``;
+  return [state, setState];
+};
+
+const useSpeech = (text, opts = {}) => {
+  const [state, setState] = useSetState({
+    isPlaying: false,
+    volume: opts.volume || 1
+  });
+
+  const uterranceRef = useRef(null);
+
+  useEffect(() => {
+    const utterance = new SpeechSynthesisUtterance(text);
+    utterance.volume = opts.volume || 1;
+    utterance.onstart = () => setState({ isPlaying: true });
+    utterance.onresume = () => setState({ isPlaying: true });
+    utterance.onend = () => setState({ isPlaying: false });
+    utterance.onpause = () => setState({ isPlaying: false });
+    uterranceRef.current = utterance;
+    window.speechSynthesis.speak(uterranceRef.current);
+  }, []);
+
+  return state;
+};
+`;
+
+const usage = `function Demo() {
+  const state = useSpeech("Hello world!");
+
+  return <pre>{JSON.stringify(state, null, 2)}</pre>;
+}`;
 
 export default {
   name,
