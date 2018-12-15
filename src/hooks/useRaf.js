593b1bb0e33add4ec591f4b7f7734b7b0a6fe547
jordynmcginnis <jordynm4@gmail.com>
2018-12-15 11:23:13 -0700

    added name

--- src/hooks/useRaf.js
+++ src/hooks/useRaf.js
@@ -1,6 +1,41 @@
-const name = ``;
+const name = `useRaf`;
 
-const implementation = ``;
+const implementation = `const useRaf = (ms = 1e12, delay = 0) => {
+  const [elapsed, set] = useState(0);
+
+  useEffect(
+    () => {
+      let raf, timerStop, start;
+
+      const onFrame = () => {
+        const time = Math.min(1, (Date.now() - start) / ms);
+        set(time);
+        loop();
+      };
+      const loop = () => {
+        raf = requestAnimationFrame(onFrame);
+      };
+      const onStart = () => {
+        timerStop = setTimeout(() => {
+          cancelAnimationFrame(raf);
+          set(1);
+        }, ms);
+        start = Date.now();
+        loop();
+      };
+      const timerDelay = setTimeout(onStart, delay);
+
+      return () => {
+        clearTimeout(timerStop);
+        clearTimeout(timerDelay);
+        cancelAnimationFrame(raf);
+      };
+    },
+    [ms, delay]
+  );
+
+  return elapsed;
+};`;
 
 const usage = ``;
 
