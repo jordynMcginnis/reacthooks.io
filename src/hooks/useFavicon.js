476922c6a25a32e5a504f15eb677987dfd23ab76
jordynmcginnis <jordynm4@gmail.com>
2018-12-15 11:03:59 -0700

    added useFavicon

--- src/hooks/useFavicon.js
+++ src/hooks/useFavicon.js
@@ -1,8 +1,25 @@
-const name = ``;
+const name = `useFavicon`;
 
-const implementation = ``;
+const implementation = `const useFavicon = href => {
+  useEffect(
+    () => {
+      const link =
+        document.querySelector("link[rel*='icon']") ||
+        document.createElement("link");
+      link.type = "image/x-icon";
+      link.rel = "shortcut icon";
+      link.href = href;
+      document.getElementsByTagName("head")[0].appendChild(link);
+    },
+    [href]
+  );
+};`;
 
-const usage = ``;
+const usage = `function Demo() {
+  useFavicon("https://cdn.sstatic.net/Sites/stackoverflow/img/favicon.ico");
+
+  return null;
+}`;
 
 export default {
   name,
