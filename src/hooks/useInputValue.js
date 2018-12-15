916e8551b67b1b00ad408d689b7b026592034eca
jordynmcginnis <jordynm4@gmail.com>
2018-12-15 11:48:53 -0700

    added name and usage

--- src/hooks/useInputValue.js
+++ src/hooks/useInputValue.js
@@ -1,8 +1,23 @@
-const name = ``;
+const name = `useInputValue`;
 
-const implementation = ``;
+const implementation = `function useInputValue(initialValue) {
+  let [value, setValue] = useState(initialValue);
 
-const usage = ``;
+  function onChange(event) {
+    setValue(event.currentTarget.value);
+  }
+
+  return {
+    value,
+    onChange
+  };
+}`;
+
+const usage = `function Demo() {
+  let name = useInputValue("Jamie");
+  // name = { value: 'Jamie', onChange: [Function] }
+  return <input {...name} />;
+}`;
 
 export default {
   name,
