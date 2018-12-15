2e2b7677c7d2535a31858f1df68a834d974fffa1
jordynmcginnis <jordynm4@gmail.com>
2018-12-15 12:04:54 -0700

    added name variable

--- src/hooks/useDeviceMotion.js
+++ src/hooks/useDeviceMotion.js
@@ -1,8 +1,47 @@
-const name =``;
+const name =`useDeviceMotion`;
 
-const implementation = ``;
+const implementation = `const [motion, setMotion] = React.useState({
+    acceleration: {
+      x: null,
+      y: null,
+      z: null
+    },
+    accelerationIncludingGravity: {
+      x: null,
+      y: null,
+      z: null
+    },
+    rotationRate: {
+      alpha: null,
+      beta: null,
+      gamma: null
+    },
+    interval: 0
+  });
 
-const usage = ``;
+  React.useEffect(() => {
+    const handle = deviceMotionEvent => {
+      setMotion(deviceMotionEvent);
+    };
+
+    window.addEventListener("devicemotion", handle);
+
+    return () => {
+      window.removeEventListener("devicemotion", handle);
+    };
+  }, []);
+
+  return motion;
+};`;
+
+const usage = `function Demo() {
+  const { acceleration, rotationRate, interval } = useDeviceMotion();
+  return (
+    <div style={{ width: "100%", height: "100%" }}>
+      <pre>{JSON.stringify({ acceleration, rotationRate, interval })}</pre>
+    </div>
+  );
+}`;
 
 export default {
   name,
