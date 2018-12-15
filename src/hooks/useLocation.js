2b6dbcd6b3346acc348e8c7f409a62bbd34325bf
jordynmcginnis <jordynm4@gmail.com>
2018-12-15 11:19:00 -0700

    updated usage

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -1,18 +1,98 @@
 const name = `useLocation`;
 
-const implementation = `const useLifecycles = (mount, unmount) => {
-  useEffect(() => {
-    if (mount) mount();
-    return () => {
-      if (unmount) unmount();
-    };
-  }, []);
+const implementation = `const isClient = typeof window === "object";
+const on = (obj, ...args) => obj.addEventListener(...args);
+const off = (obj, ...args) => obj.removeEventListener(...args);
+
+const patchHistoryMethod = method => {
+  const original = history[method];
+
+  history[method] = function(state) {
+    const result = original.apply(this, arguments);
+    const event = new Event(method.toLowerCase());
+
+    event.state = state;
+
+    window.dispatchEvent(event);
+
+    return result;
+  };
 };
-`;
+
+if (isClient) {
+  patchHistoryMethod("pushState");
+  patchHistoryMethod("replaceState");
+}
+
+const useLocation = () => {
+  const buildState = trigger => {
+    const { state, length } = history;
+
+    const {
+      hash,
+      host,
+      hostname,
+      href,
+      origin,
+      pathname,
+      port,
+      protocol,
+      search
+    } = location;
+
+    return {
+      trigger,
+      state,
+      length,
+      hash,
+      host,
+      hostname,
+      href,
+      origin,
+      pathname,
+      port,
+      protocol,
+      search
+    };
+  };
+
+  const [state, setState] = useState(
+    isClient
+      ? buildState("load")
+      : {
+          trigger: "load",
+          length: 1
+        }
+  );
+
+  const onChange = trigger => setState(buildState(trigger));
+
+  const onPopstate = () => onChange("popstate");
+  const onPushstate = () => onChange("pushstate");
+  const onReplacestate = () => onChange("replacestate");
+
+  useEffect(
+    () => {
+      on(window, "popstate", onPopstate);
+      on(window, "pushstate", onPushstate);
+      on(window, "replacestate", onReplacestate);
+
+      return () => {
+        off(window, "popstate", onPopstate);
+        off(window, "pushstate", onPushstate);
+        off(window, "replacestate", onReplacestate);
+      };
+    },
+    [0]
+  );
+
+  return state;
+};`;
 
 const usage = `function Demo() {
-  useLifecycles(() => console.log("MOUNTED"), () => console.log("UNMOUNTED"));
-  return null;
+  const state = useLocation();
+
+  return <pre>{JSON.stringify(state, null, 2)}</pre>;
 }`;
 
 export default {
