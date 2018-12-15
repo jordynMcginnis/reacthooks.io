abff5a1fae01f602553b8c40a738da07dba55eb7
jordynmcginnis <jordynm4@gmail.com>
2018-12-15 11:24:51 -0700

    added usage

--- src/hooks/useSetstate.js
+++ src/hooks/useSetstate.js
@@ -1,8 +1,26 @@
-const name = ``;
+const name = `useSetstate`;
 
-const implementation = ``;
+const implementation = `const useSetState = (initialState = {}) => {
+  const [state, set] = useState(initialState);
+  const setState = patch => {
+    Object.assign(state, patch);
+    set(state);
+  };
 
-const usage = ``;
+  return [state, setState];
+};`;
+
+const usage = `function Demo() {
+  const [state, setState] = useSetState({});
+
+  return (
+    <div>
+      <pre>{JSON.stringify(state, null, 2)}</pre>
+      <button onClick={() => setState({ hello: "world" })}>hello</button>
+      <button onClick={() => setState({ foo: "bar" })}>foo</button>
+    </div>
+  );
+}`;
 
 export default {
   name,
