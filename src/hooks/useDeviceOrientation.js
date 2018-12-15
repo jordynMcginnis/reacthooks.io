cd7b22036623d50ba7602f6f0be745dc49c36625
jordynmcginnis <jordynm4@gmail.com>
2018-12-15 11:34:01 -0700

    added usage and name

--- src/hooks/useDeviceOrientation.js
+++ src/hooks/useDeviceOrientation.js
@@ -1,8 +1,44 @@
 const name = `use`;
 
-const implementation = ``;
+const implementation = `function useDeviceOrientation() {
+  const [deviceOrientation, setDeviceOrientation] = useState({
+    absolute: false,
+    alpha: null,
+    beta: null,
+    gamma: null
+  });
 
-const usage = ``;
+  function handleDeviceOrientation(event) {
+    setDeviceOrientation({
+      absolute: event.absolute,
+      alpha: event.alpha,
+      beta: event.beta,
+      gamma: event.gamma
+    });
+  }
+
+  useEffect(() => {
+    window.addEventListener("deviceorientation", handleDeviceOrientation, true);
+
+    return () => {
+      window.removeEventListener("deviceorientation", handleDeviceOrientation);
+    };
+  }, []);
+
+  return deviceOrientation;
+}`;
+
+const usage = `function Demo() {
+  let value = useDeviceOrientation();
+  return (
+    <div>
+      <p>Absolute: {value.absolute}</p>
+      <p>Alpha: {value.alpha}</p>
+      <p>Beta: {value.beta}</p>
+      <p>Gamma: {value.gamma}</p>
+    </div>
+  );
+}`;
 
 export default {
   name,
