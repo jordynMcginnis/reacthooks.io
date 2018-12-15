58f1c696804a544f0ef786caa461c9795922c875
jordynmcginnis <jordynm4@gmail.com>
2018-12-15 12:06:31 -0700

    usage added to hook

--- src/hooks/useWindowMousePosition.js
+++ src/hooks/useWindowMousePosition.js
@@ -1,8 +1,38 @@
-const name =``;
+const name =`useWindowMousePosition`;
 
-const implementation = ``;
+const implementation = `function useWindowMousePosition() {
+  let [WindowMousePosition, setWindowMousePosition] = useState({
+    x: null,
+    y: null
+  });
 
-const usage = ``;
+  function handleMouseMove(e) {
+    setWindowMousePosition({
+      x: e.pageX,
+      y: e.pageY
+    });
+  }
+
+  useEffect(() => {
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  });
+
+  return WindowMousePosition;
+}`;
+
+const usage = `function Demo() {
+  let { x, y } = useWindowMousePosition();
+  return (
+    <div style={{ width: "100%", height: "100%" }}>
+      <pre>{JSON.stringify({ x, y })}</pre>
+    </div>
+  );
+}
+`;
 
 export default {
   name,
--- src/hooks/useWindowSize.js
+++ src/hooks/useWindowSize.js
@@ -1,8 +1,35 @@
-const name =``;
+const name =`useWindowSize`;
 
-const implementation = ``;
+const implementation = `function getSize() {
+  return {
+    innerHeight: window.innerHeight,
+    innerWidth: window.innerWidth,
+    outerHeight: window.outerHeight,
+    outerWidth: window.outerWidth
+  };
+}
 
-const usage = ``;
+function useWindowSize() {
+  let [windowSize, setWindowSize] = useState(getSize());
+
+  function handleResize() {
+    setWindowSize(getSize());
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return windowSize;
+}`;
+
+const usage = `function Demo() {
+  const size = useWindowSize();
+  return <div>{size.innerWidth}</div>;
+}`;
 
 export default {
   name,
