f3da454ce733c0a9a8c01f1a103ce16599365729
jordynmcginnis <jordynm4@gmail.com>
2018-12-15 11:49:36 -0700

    usage added

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,8 +1,34 @@
-const name = ``;
+const name = `useLocalStorage`;
 
-const implementation = ``;
+const implementation = `function useLocalStorage(key) {
+  let localStorageItem;
+  if (key) {
+    localStorageItem = localStorage[key];
+  }
+  const [localState, updateLocalState] = useState(localStorageItem);
+  function syncLocalStorage(event) {
+    if (event.key === key) {
+      updateLocalState(event.newValue);
+    }
+  }
+  useEffect(() => {
+    window.addEventListener("storage", syncLocalStorage);
+    return () => {
+      window.removeEventListener("storage", syncLocalStorage);
+    };
+  }, []);
+  return localState;
+}
+`;
 
-const usage = ``;
+const usage = `function Demo() {
+  let name = useLocalStorage("name"); // send the key to be tracked.
+  return (
+    <div>
+      <h1>{name}</h1>
+    </div>
+  );
+}`;
 
 export default {
   name,
