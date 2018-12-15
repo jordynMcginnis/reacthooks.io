4668e1da1e193d218b41b348878cefe5f0ffe9f6
jordynmcginnis <jordynm4@gmail.com>
2018-12-15 11:06:35 -0700

    added useHover implmentation

--- src/hooks/useHover.js
+++ src/hooks/useHover.js
@@ -1,6 +1,28 @@
 const name = `useHover`;
 
-const implementation = ``;
+const implementation = `const useHover = element => {
+  const [state, setState] = useState(false);
+  const noop = () => {};
+  const onMouseEnter = originalOnMouseEnter => event => {
+    (originalOnMouseEnter || noop)(event);
+    setState(true);
+  };
+  const onMouseLeave = originalOnMouseLeave => event => {
+    (originalOnMouseLeave || noop)(event);
+    setState(false);
+  };
+
+  if (typeof element === "function") {
+    element = element(state);
+  }
+
+  const el = React.cloneElement(element, {
+    onMouseEnter: onMouseEnter(element.props.onMouseEnter),
+    onMouseLeave: onMouseLeave(element.props.onMouseLeave)
+  });
+
+  return [el, state];
+};`;
 
 const usage = ``;
 
