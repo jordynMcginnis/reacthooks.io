2d2a4298532990240786d36d987407dbc27ed2a0
jordynmcginnis <jordynm4@gmail.com>
2018-12-30 13:21:53 -0700

    instaSearch added

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { hookNames } from './hooks/index.js';
+import Search from './Search.js';
+import { InstantSearch } from 'react-instantsearch-dom';
+
 
 export function SideBar () {
   return (
     <div className='sideBar'>
-    <h2 className='title'><span className='r-title'>react</span>Hooks.io</h2>
-      <ul>
-        {hookNames.map((hook)=>{
-          return (
-            <li key={hook}>
-              <Link to={`/${hook}`}>{hook}</Link>
-            </li>
-          )
-        })}
-      </ul>
+      <h2 className='title'><span className='r-title'>react</span>Hooks.io</h2>
+
+      <div className='algolia-container'>
+        <InstantSearch
+          appId="HL2G9DSLG4"
+          apiKey="b3debf5c2d7872f333a5e14e776b091d"
+          indexName="hooks"
+        >
+          <Search/>
+        </InstantSearch>
+      </div>
     </div>
   );
 }
