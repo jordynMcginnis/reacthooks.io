bb4ce5cf927551a1812d8730f8326aa095e9c4a5
jordynmcginnis <jordynm4@gmail.com>
2019-01-03 15:57:25 -0700

    added description

--- src/hooks/useOnUnmount.js
+++ src/hooks/useOnUnmount.js
@@ -12,9 +12,12 @@ const usage = `function Demo() {
 
 const url = `https://github.com/kitze/react-hanger`;
 
+const description = `This hook is used for the unMount lifecycle.`;
+
 export default {
   name,
   implementation,
   usage,
-  url
+  url,
+  description
 }
