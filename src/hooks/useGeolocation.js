1baf618268ac78bbfb5a678dea2b0b15d4fa3796
jordynmcginnis <jordynm4@gmail.com>
2019-01-03 13:26:17 -0700

    updated description

--- src/hooks/useGeolocation.js
+++ src/hooks/useGeolocation.js
@@ -53,7 +53,7 @@ const usage = `function Demo() {
 
 const url = `https://github.com/streamich/react-use/blob/master/docs/useGeolocation.md`;
 
-const description = `React sensor hook that tracks user's geographic location.`;
+const description = `This hook tracks a user's geographic location.`;
 
 export default {
   name,
