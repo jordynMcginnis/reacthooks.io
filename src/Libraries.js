3fcc78fde48683eb5727d829d369021291858817
jordynmcginnis <jordynm4@gmail.com>
2019-01-06 10:46:11 -0700

    added library

---
+++ src/Libraries.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export function Libraries () {
+  return (
+    <div>
+      <ul>
+        <li><a href='https://reactjs.org/docs/hooks-intro.html'>Built-In</a></li>
+        <li><a href='https://github.com/diegohaz/constate'>Constate</a></li>
+        <li><a href='https://github.com/kitze/react-hanger'>React-Hanger</a></li>
+        <li><a href='https://github.com/kalcifer/react-powerhooks'>React-Powerhooks</a></li>
+        <li><a href='https://github.com/streamich/react-use'>React-Use</a></li>
+        <li><a href='https://github.com/rehooks'>Rehooks</a></li>
+        <li><a href='https://github.com/palmerhq/the-platform/'>The-Platform</a></li>
+      </ul>
+    </div>
+  )
+}
