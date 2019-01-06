7f80487f41d71c853efaa63c58b37ab6cb5b1930
jordynmcginnis <jordynm4@gmail.com>
2019-01-06 10:43:36 -0700

    added navbar

--- src/Nav.js
+++ src/Nav.js
@@ -1,1 +1,13 @@
-Nav.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export function Nav () {
+  return (
+    <div className='nav'>
+      <ul>
+        <Link to="/home"><li>Copy and Paste</li></Link>
+        <Link to="/libraries"><li>Libraries</li></Link>
+      </ul>
+    </div>
+  )
+}
