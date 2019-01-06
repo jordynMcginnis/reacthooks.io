d9955a3a0ea48ddab4a5f16d7e3efe8887d836cc
jordynmcginnis <jordynm4@gmail.com>
2019-01-06 14:19:22 -0700

    added a href

--- src/Libraries.js
+++ src/Libraries.js
@@ -3,36 +3,50 @@ import React from 'react';
 export function Libraries () {
   return (
     <div className='libraries'>
-      <h1>Libraries</h1>
+      <h1>LIBRARIES</h1>
       <ul>
-        <li>
-          <a href='https://reactjs.org/docs/hooks-intro.html'>Built-In</a>
-          <span></span>
-        </li>
-        <li>
-          <a href='https://github.com/diegohaz/constate'>Constate</a>
-          <span>Scalable state manager using React Hooks & Context</span>
-        </li>
-        <li>
-          <a href='https://github.com/kitze/react-hanger'>React-Hanger</a>
-          <span>A small collection of useful hooks for React 16.7</span>
-        </li>
-        <li>
-          <a href='https://github.com/kalcifer/react-powerhooks'>React-Powerhooks</a>
-          <span>Hooks api for react-powerplug components</span>
-        </li>
-        <li>
-          <a href='https://github.com/streamich/react-use'>React-Use</a>
-          <span>Collection of essential React Hooks</span>
-        </li>
-        <li>
-          <a href='https://github.com/rehooks'>Rehooks</a>
-          <span>React Hooks for Everything</span>
-        </li>
-        <li>
-          <a href='https://github.com/palmerhq/the-platform/'>The-Platform</a>
-          <span>Web. Components.</span>
-        </li>
+        <a href='https://reactjs.org/docs/hooks-intro.html'>
+          <li>
+            Built-In
+            <span></span>
+          </li>
+        </a>
+        <a href='https://github.com/diegohaz/constate'>
+          <li>
+            Constate
+            <span>Scalable state manager using React Hooks & Context</span>
+          </li>
+        </a>
+        <a href='https://github.com/kitze/react-hanger'>
+          <li>
+            React-Hanger
+            <span>A small collection of useful hooks for React 16.7</span>
+          </li>
+        </a>
+        <a href='https://github.com/kalcifer/react-powerhooks'>
+          <li>
+            React-Powerhooks
+            <span>Hooks api for react-powerplug components</span>
+          </li>
+        </a>
+        <a href='https://github.com/streamich/react-use'>
+          <li>
+            React-Use
+            <span>Collection of essential React Hooks</span>
+          </li>
+        </a>
+        <a href='https://github.com/rehooks'>
+          <li>
+            Rehooks
+            <span>React Hooks for Everything</span>
+          </li>
+        </a>
+        <a href='https://github.com/palmerhq/the-platform/'>
+          <li>
+            The-Platform
+            <span>Web. Components.</span>
+          </li>
+        </a>
       </ul>
     </div>
   )
