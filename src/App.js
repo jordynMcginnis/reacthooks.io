b36f9a034d38f30987682f40287be67eef09cd28
jordynmcginnis <jordynm4@gmail.com>
2019-01-06 10:41:48 -0700

    added another route

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import './index.css';
 import { SideBar } from './SideBar.js';
 import { Hook } from './Hook.js';
+import { Nav } from './Nav.js';
+import { Libraries } from './Libraries.js';
 
 function App() {
   return (
     <Router>
       <div className='app'>
-        <Route path='/' component={SideBar}/>
-        <Route exact path='/:id' component={Hook} />
+        <Route path='/' component={Nav}/>
+        <Route exact path='/home' component={SideBar}/>
+        <Route exact path='/libraries' component={Libraries}/>
+        <Route exact path='/hooks/:id' component={Hook}/>
       </div>
     </Router>
   );
