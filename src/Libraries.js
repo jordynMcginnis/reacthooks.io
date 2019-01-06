e64db0fbcf057564e723e084e28d42c87e7bd3f1
jordynmcginnis <jordynm4@gmail.com>
2019-01-06 12:06:57 -0700

    styled libraries

--- src/Libraries.js
+++ src/Libraries.js
@@ -2,15 +2,37 @@ import React from 'react';
 
 export function Libraries () {
   return (
-    <div>
+    <div className='libraries'>
+      <h1>Libraries</h1>
       <ul>
-        <li><a href='https://reactjs.org/docs/hooks-intro.html'>Built-In</a></li>
-        <li><a href='https://github.com/diegohaz/constate'>Constate</a></li>
-        <li><a href='https://github.com/kitze/react-hanger'>React-Hanger</a></li>
-        <li><a href='https://github.com/kalcifer/react-powerhooks'>React-Powerhooks</a></li>
-        <li><a href='https://github.com/streamich/react-use'>React-Use</a></li>
-        <li><a href='https://github.com/rehooks'>Rehooks</a></li>
-        <li><a href='https://github.com/palmerhq/the-platform/'>The-Platform</a></li>
+        <li>
+          <a href='https://reactjs.org/docs/hooks-intro.html'>Built-In</a>
+          <span></span>
+        </li>
+        <li>
+          <a href='https://github.com/diegohaz/constate'>Constate</a>
+          <span>Scalable state manager using React Hooks & Context</span>
+        </li>
+        <li>
+          <a href='https://github.com/kitze/react-hanger'>React-Hanger</a>
+          <span>A small collection of useful hooks for React 16.7</span>
+        </li>
+        <li>
+          <a href='https://github.com/kalcifer/react-powerhooks'>React-Powerhooks</a>
+          <span>Hooks api for react-powerplug components</span>
+        </li>
+        <li>
+          <a href='https://github.com/streamich/react-use'>React-Use</a>
+          <span>Collection of essential React Hooks</span>
+        </li>
+        <li>
+          <a href='https://github.com/rehooks'>Rehooks</a>
+          <span>React Hooks for Everything</span>
+        </li>
+        <li>
+          <a href='https://github.com/palmerhq/the-platform/'>The-Platform</a>
+          <span>Web. Components.</span>
+        </li>
       </ul>
     </div>
   )
