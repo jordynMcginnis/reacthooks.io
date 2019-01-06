c97fbd5dbda06a2698dc894c3521d6c16a69379b
jordynmcginnis <jordynm4@gmail.com>
2019-01-06 16:28:47 -0700

    added span around link titles

--- src/Libraries.js
+++ src/Libraries.js
@@ -7,43 +7,43 @@ export function Libraries () {
       <ul>
         <a href='https://reactjs.org/docs/hooks-intro.html'>
           <li>
-            Built-In
+            <span className='l-title'>Built-In</span>
             <span></span>
           </li>
         </a>
         <a href='https://github.com/diegohaz/constate'>
           <li>
-            Constate
+            <span className='l-title'>Constate</span>
             <span>Scalable state manager using React Hooks & Context</span>
           </li>
         </a>
         <a href='https://github.com/kitze/react-hanger'>
           <li>
-            React-Hanger
+            <span className='l-title'>React-Hanger</span>
             <span>A small collection of useful hooks for React 16.7</span>
           </li>
         </a>
         <a href='https://github.com/kalcifer/react-powerhooks'>
           <li>
-            React-Powerhooks
+            <span className='l-title'>React-Powerhooks</span>
             <span>Hooks api for react-powerplug components</span>
           </li>
         </a>
         <a href='https://github.com/streamich/react-use'>
           <li>
-            React-Use
+            <span className='l-title'>React-Use</span>
             <span>Collection of essential React Hooks</span>
           </li>
         </a>
         <a href='https://github.com/rehooks'>
           <li>
-            Rehooks
+            <span className='l-title'>Rehooks</span>
             <span>React Hooks for Everything</span>
           </li>
         </a>
         <a href='https://github.com/palmerhq/the-platform/'>
           <li>
-            The-Platform
+            <span className='l-title'>The-Platform</span>
             <span>Web. Components.</span>
           </li>
         </a>
