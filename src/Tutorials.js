487c588d9208df2623cf614de584fe916c4498c1
Tyler McGinnis <tylermcginnis33@gmail.com>
2019-01-29 12:31:45 -0700

    Add official hooks intro

--- src/Tutorials.js
+++ src/Tutorials.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const tuts = [{
-    name: 'Todo 1',
-    href: 'https://google.com',
-    description: 'description',
+    name: 'Introducing Hooks',
+    href: 'https://reactjs.org/docs/hooks-intro.html',
+    description: 'The official introduction to Hooks on the React blog.',
   }, {
     name: 'Todo 2',
     href: 'https://google.com',
